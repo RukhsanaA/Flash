add55d2429a966c878bd455a21b27464299706a0
RukhsanaA <39635159+RukhsanaA@users.noreply.github.com>
2025-08-10 17:40:10 +0100

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,4 +1,5 @@
 let flashcard = document.getElementById('flashcard');
+
 flashcard.addEventListener('click', function() {
     // This is where we'll put the instructions for what to do when the flashcard is clicked
 });
